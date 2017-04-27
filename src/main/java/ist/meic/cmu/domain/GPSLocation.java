08a2a3956d484e6d87887110dcb2ffe90641e10e
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2017-04-27 03:45:24 +0100

    radius restored!

--- src/main/java/ist/meic/cmu/domain/GPSLocation.java
+++ src/main/java/ist/meic/cmu/domain/GPSLocation.java
@@ -10,8 +10,16 @@ public class GPSLocation extends Location {
 
     private double latitude;
     private double longitude;
+    private double radius;
 
     public GPSLocation(){
+    }
+
+    public GPSLocation(String name, double latitude, double longitude, double radius) {
+        super(name);
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.radius = radius;
     }
 
     public GPSLocation(double latitude, double longitude) {
@@ -36,4 +44,11 @@ public class GPSLocation extends Location {
         this.longitude = longitude;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
