1fc278d3e0f6efdb675aef925638b43b5e8f2e26
drlferreira <diogo.lopes.ferreira@tecnico.ulisboa.pt>
2017-05-13 18:09:09 +0100

    haversine

--- src/main/java/ist/meic/cmu/domain/GPSLocation.java
+++ src/main/java/ist/meic/cmu/domain/GPSLocation.java
@@ -22,6 +22,36 @@ public class GPSLocation extends Location {
         this.radius = radius;
     }
 
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     * <p>
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * el2 End altitude in meters
+     *
+     * @returns Distance in Meters
+     */
+    public static double haversine(double lat1, double lat2, double lon1,
+                                   double lon2, double el1, double el2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
+    }
+
     public double getLatitude() {
         return latitude;
     }
@@ -46,16 +76,12 @@ public class GPSLocation extends Location {
         this.radius = radius;
     }
 
-
     @Override
     public boolean equals(Object o){
         if(o != null && o instanceof GPSLocation){
             GPSLocation toCompare = (GPSLocation) o;
-            double x = Math.abs(this.latitude) - Math.abs(toCompare.getLatitude());
-            double y = Math.abs(this.longitude) - Math.abs(toCompare.getLongitude());
-            double d = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
-            System.out.println(d <= (radius) / 2);
-            return d <= (radius/2);
+            double haversine = haversine(this.latitude, toCompare.getLatitude(), this.longitude, toCompare.getLongitude(), 0, 0);
+            return haversine <= (radius/2);
         }
         return false;
     }
